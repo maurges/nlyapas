79a99177e95db043599be3f25217f923d36424f0
d86leader <d86leader@gmail.com>
2022-09-29 21:25:43 +0200

    Naive compiler works

--- runtime/nlyapas_rt.c
+++ runtime/nlyapas_rt.c
@@ -22,7 +22,10 @@ void __nlyapas_rt__seed_rand(uint64_t seed) {
 }
 
 struct Complex* __nlyapas_rt__alloc_complex(uint64_t size) {
-    return malloc(sizeof(struct Complex) + size*8);
+    struct Complex* r = malloc(sizeof(struct Complex) + size*8);
+    r->capacity = size;
+    r->size = 0;
+    return r;
 }
 
 void __nlyapas_rt__free_complex(struct Complex* p) {
@@ -40,5 +43,26 @@ struct Complex* __nlyapas_rt__realloc_complex(struct Complex* old, uint64_t size
 }
 
 void __nlyapas_rt__print_complex(struct Complex* p) {
-    printf("%s", (char*)p->data);
+    fwrite(p->data, 1, p->size, stdout);
+}
+
+void __nlyapas_rt__print_string(char* p, uint64_t size) {
+    fwrite(p, 1, size, stdout);
+}
+
+void __nlyapas_rt__read_complex(struct Complex* p) {
+    size_t r = fread(p->data, 1, p->capacity, stdin);
+    p->size = r;
+}
+
+uint64_t __nlyapas_rt__count_trailing_zeroes(uint64_t x) {
+    if (x == 0) {
+        return 64;
+    }
+    uint64_t r = 0;
+    while (x != 0) {
+        x >>= 1;
+        r += 1;
+    }
+    return r;
 }
